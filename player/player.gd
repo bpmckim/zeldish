b6841c59602cda50d10ab41065fd0a749458ca65
Brian McKim <brian@wodikadevine.com>
2023-10-04 15:39:37 -0400

    through chapter 7

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
 @export var speed: int = 35
+@onready var animations = $AnimationPlayer
 
 func getInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
 	
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+	
+		animations.play("walk" + direction)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	getInput()
 	move_and_slide()
+	updateAnimation()
